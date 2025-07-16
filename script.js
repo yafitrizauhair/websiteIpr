f3e4257ff3246f7abbaebdbb06817c1e0d9745ac
Zahran03 <zalfarizi502@gmail.com>
2025-07-17 06:46:11 +0700

    feat: create slide for mitra

--- script.js
+++ script.js
@@ -35,5 +35,22 @@ document.addEventListener("click", function (e) {
   }
 });
 
+const slides = ["slides1", "slides2"];
+const logos = ["logos1", "logos2"];
+
 var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
+
+var copy2 = document.getElementById("slides2").cloneNode(true);
+document.getElementById("logos2").appendChild(copy2);
+
+const copy3 = document.getElementById("slides3").cloneNode(true);
+document.getElementById("logos3").appendChild(copy3);
+
+const copy4 = document.getElementById("slides4").cloneNode(true);
+document.getElementById("logos4").appendChild(copy4);
+const copy5 = document.getElementById("slides5").cloneNode(true);
+document.getElementById("logos5").appendChild(copy5);
+
+const copy6 = document.getElementById("slides6").cloneNode(true);
+document.getElementById("logos6").appendChild(copy6);
